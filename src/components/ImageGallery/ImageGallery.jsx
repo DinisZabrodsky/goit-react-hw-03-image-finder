75ebe45729da014ea3d4e36c70a13fd519ac88af
Dinis Zabrodsky <DinisZabrodsky@gmail.com>
2023-05-20 17:39:46 +0300

    install

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,5 +1,5 @@
 import { Component } from 'react'
-import {getImage} from '../../api/getImage'
+
 import {ImageGalleryItem} from '../ImageGalleryItem/ImageGalleryItem'
 import {Button} from '../Button/Button'
 import {Loader} from '../Loader/Loader'
@@ -8,100 +8,21 @@ import PropTypes from 'prop-types';
 import IGCss from './ImageGallery.module.css'
 
 export class ImageGallery extends Component {
-    state = {
-        imageBase: [],
-        total: 0,
-        page: 1,
-        new: true,
-        load: false,
-        error: false
-    }
-
-componentDidUpdate = (prevProps, prevState) => {
-
-        if (prevProps.search !== this.props.search || prevState.page !== this.state.page) {
-            this.setState({load: true})
-
-            getImage({query: this.props.search, page: this.state.page})
-            .then(response =>{
-
-                if(response.total === 0) {
-                    alert(`Нажаль за запитом "${this.props.search}" нічого не знайдено`)
-                }
-
-                if (prevProps.search !== this.props.search) {
-                    return this.setState({
-                        page: 1,
-                        total: response.total,
-                        imageBase: response.hits,
-                        new: true,
-                        
-                    }) 
-                }
-
-                if(prevState.page !== this.state.page && !this.state.new) {
-                    return this.setState({imageBase: [...prevState.imageBase, ...response.hits]})
-                }
-
-                
-            }).catch(() => this.setState({error: true}))
-                .finally(()=>{
-                    return setTimeout(() => {
-                            this.setState({load: false})
-                        }, 250);
-                    
-                })
-
-            // .finally(this.setState({load: false}))
-            
-                
-        }  
-
-        // if (prevProps.search !== this.props.search) {
-        //     this.setState({load: true})
-        //     return getImage({query: this.props.search, page: 1})
-        //     .then(response =>{
-        //         return this.setState({
-        //                         page: 1,
-        //                         total: response.total,
-        //                         imageBase: response.hits,
-        //                         new: true,
-        //                     }) 
-        //     }).catch(console.log("error")).finally(this.setState({load: false}))
-        // }  
-        
-        // if(prevState.page !== this.state.page && !this.state.new) {
-        //     this.setState({load: true})
-        //     return getImage({query: this.props.search, page: this.state.page})
-        //     .then(response =>{
-        //         return this.setState({imageBase: [...prevState.imageBase, ...response.hits] })
-        //     }).catch(console.log("error")).finally(this.setState({load: false}))
-        // }
-
-    
-}   
-
-    addMore = () => {
-        this.setState( prev =>{return {
-            page: prev.page + 1,
-            new: false
-            }})
-    }
-
+    state = {}
 
     render() {
-        if(this.state.load) {
+        if(this.props.load) {
             return <Loader />
         }
 
         return <>
                 <ul className={IGCss.ImageGallery}>
-                    {this.state.imageBase.map(({id, webformatURL, tags, largeImageURL}) => {
+                    {this.props.imageBase.map(({id, webformatURL, tags, largeImageURL}) => {
                         return <ImageGalleryItem key={id} webformatURL={webformatURL} tags={tags} imageForModal={largeImageURL} openModal={this.props.openModal}/>
                     })}
                 </ul>
 
-                { this.state.imageBase.length > 0 && this.state.imageBase.length < this.state.total && <Button addMore={this.addMore}/>} 
+                { this.props.imageBase.length > 0 && this.props.imageBase.length < this.props.total && <Button addMore={this.props.addMore}/>} 
 
              
       </>}
